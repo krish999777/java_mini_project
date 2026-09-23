52276bb847827ef23c2f7e6a64a2634b7a44553d
Krish Shah <krishcshah09@gmail.com>
2026-09-23 22:05:09 +0530

    testing in main and compilation

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
 //javac -cp ".:/Users/krishshah/all coding stuff/java_mini_project/lib/postgresql-42.7.13.jar:/Users/krishshah/all coding stuff/java_mini_project" Main.java
 //java -cp ".:/Users/krishshah/all coding stuff/java_mini_project/lib/postgresql-42.7.13.jar:/Users/krishshah/all coding stuff/java_mini_project" Main
 import myExceptions.LoginException;
-import session.Session;
 
 class Main{
     public static void main(String args[]){
         try{
             Login.login("krish","krish123");
-            System.out.println("ID="+Session.getId());
-            System.out.println("Role="+Session.getRole());
+            StudentProfile.createProfile("krish", "krish@gmail.com", "1234567890", "sbmp", "", 1, "", "", "");
         }catch(LoginException e){
             System.out.println(e);
         }catch(Exception e){
