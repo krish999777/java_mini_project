b575c0c0370e40e33e72d99b87547db02aaf2efd
Krish Shah <krishcshah09@gmail.com>
2026-09-23 19:42:42 +0530

    added education data model for profile

---
+++ data/Education.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Education {
+    public String institution;
+    public String degree;
+    public int startYear;
+    public int endYear;
+    public String grade;
+    public Education(){}
+    public Education(String i,String d,int s,int e,String g){
+        institution=i;
+        degree=d;
+        startYear=s;
+        endYear=e;
+        grade=g;
+    }
+}
