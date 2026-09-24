6fd6d6a86387b576527278dd967afd7aa69c8eed
Krish Shah <krishcshah09@gmail.com>
2026-09-24 23:18:26 +0530

    removed all .class files and tested getProfile in main

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 //javac -cp ".:/Users/krishshah/all coding stuff/java_mini_project/lib/postgresql-42.7.13.jar:/Users/krishshah/all coding stuff/java_mini_project" Main.java
 //java -cp ".:/Users/krishshah/all coding stuff/java_mini_project/lib/postgresql-42.7.13.jar:/Users/krishshah/all coding stuff/java_mini_project" Main
 import myExceptions.LoginException;
+import data.StudentProfileModel;
 
 class Main{
     public static void main(String args[]){
         try{
-            Login.login("krish","krish123");
-            StudentProfile.createProfile("krish", "krish@gmail.com", "1234567890", "sbmp", "", 1, "", "", "");
+            Login.login("krish2","krish123");
+            StudentProfileModel s=StudentProfile.getProfile();
+            System.out.println(s);
         }catch(LoginException e){
             System.out.println(e);
         }catch(Exception e){
