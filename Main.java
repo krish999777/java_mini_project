318dee9d1e4a33af9e8873bb066370d4d85c847d
Krish Shah <krishcshah09@gmail.com>
2026-09-26 16:41:38 +0530

    testing in main for removeSkills

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ class Main{
     public static void main(String args[]){
         try{
             Login.login("krish2","krish123");
-            // Skills.addSkill("MongoDB");
+            Skills.removeSkill("MongoDB");
             // StudentProfile.updateProfile("krish","krish2@gmail.com","","SBMP","IT",-1,"Full stack dev with experience in ai engineering,cybersecurity,devops and iot","https://github.com/krish999777","https://www.linkedin.com/in/krish-shah09");
             // System.out.println(StudentProfile.getProfile());
             String s[]=Skills.getSkills();
